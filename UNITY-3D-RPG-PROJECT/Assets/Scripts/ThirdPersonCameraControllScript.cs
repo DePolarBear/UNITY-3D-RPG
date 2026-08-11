d9c3ce314ac92f8822b721ab1eb283764f472cc2
Tomáš Ranuša <tomas.ranusa@gmail.com>
2026-08-11 14:21:43 +0200

    Camera movement and zoom script

---
+++ UNITY-3D-RPG-PROJECT/Assets/Scripts/ThirdPersonCameraControllScript.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using Unity.Cinemachine;
+
+public class ThirdPersonCameraControllScript : MonoBehaviour
+{
+    [SerializeField] private float zoomSpeed = 2f;
+    [SerializeField] private float zoomLerpSpeed = 10f;
+    [SerializeField] private float minDistance = 3f;
+    [SerializeField] private float maxDistance = 15f;
+
+    public InputActionReference zoom;
+
+    private CinemachineOrbitalFollow orbital;
+    private Vector2 scrollDelta;
+
+    private float targetZoom;
+    private float currentZoom;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+
+        orbital = GetComponent<CinemachineOrbitalFollow>();
+
+        targetZoom = currentZoom = orbital.Radius;
+    }
+
+    private void Update()
+    {
+        scrollDelta = zoom.action.ReadValue<Vector2>();
+
+        if (Mathf.Abs(scrollDelta.y) > 0.01f)
+        {
+            targetZoom -= Mathf.Sign(scrollDelta.y) * zoomSpeed;
+            targetZoom = Mathf.Clamp(targetZoom, minDistance, maxDistance);
+        }
+
+        currentZoom = Mathf.Lerp(currentZoom, targetZoom, zoomLerpSpeed * Time.deltaTime);
+        orbital.Radius = currentZoom;
+    }
+}
